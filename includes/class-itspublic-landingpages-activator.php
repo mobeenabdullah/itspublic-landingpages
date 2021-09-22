0c3bcb7bb3651ea843faf9473cbfc2d2395ee073
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-22 17:04:52 +0500

    Added base files.

---
+++ includes/class-itspublic-landingpages-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://mobeenabdullah.com
+ * @since      1.0.0
+ *
+ * @package    Itspublic_Landingpages
+ * @subpackage Itspublic_Landingpages/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Itspublic_Landingpages
+ * @subpackage Itspublic_Landingpages/includes
+ * @author     Mobeen Abdullah <mobeenabdullah@gmail.com>
+ */
+class Itspublic_Landingpages_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-itspublic-landingpages-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://mobeenabdullah.com
+ * @since      1.0.0
+ *
+ * @package    Itspublic_Landingpages
+ * @subpackage Itspublic_Landingpages/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Itspublic_Landingpages
+ * @subpackage Itspublic_Landingpages/includes
+ * @author     Mobeen Abdullah <mobeenabdullah@gmail.com>
+ */
+class Itspublic_Landingpages_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
